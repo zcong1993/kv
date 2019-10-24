2b2d87ec429fe9c20b903ae572d734f552ddfa97
zcong1993 <zhangcong1992@gmail.com>
2019-10-24 12:09:59 +0800

    feat: add dynamodb store

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/zcong1993/kv
 go 1.13
 
 require (
+	github.com/aws/aws-sdk-go v1.25.18
 	github.com/stretchr/testify v1.4.0
 	go.etcd.io/bbolt v1.3.3
 	golang.org/x/sys v0.0.0-20191023151326-f89234f9a2c2 // indirect
---
+++ pkg/dynamo/dynamo.go
@@ -0,0 +1,83 @@
+package dynamo
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+)
+
+type Store struct {
+	dynamo *dynamodb.DynamoDB
+	name   string
+}
+
+type Item struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
+func NewStore(name string, cfgs ...*aws.Config) *Store {
+	sess := session.Must(session.NewSession())
+	svc := dynamodb.New(sess, cfgs...)
+
+	return &Store{
+		dynamo: svc,
+		name:   name,
+	}
+}
+
+func (s *Store) Name() string {
+	return s.name
+}
+
+func (s *Store) Put(key string, value []byte) error {
+	item := &Item{
+		Key:   key,
+		Value: string(value),
+	}
+
+	av, err := dynamodbattribute.MarshalMap(item)
+	if err != nil {
+		return err
+	}
+
+	_, err = s.dynamo.PutItem(&dynamodb.PutItemInput{
+		Item:      av,
+		TableName: &s.name,
+	})
+
+	return err
+}
+
+func (s *Store) Get(key string) (value []byte, err error) {
+	result, err := s.dynamo.GetItem(&dynamodb.GetItemInput{
+		TableName: &s.name,
+		Key: map[string]*dynamodb.AttributeValue{
+			"key": {
+				S: aws.String(key),
+			},
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	var item Item
+	err = dynamodbattribute.UnmarshalMap(result.Item, &item)
+	return []byte(item.Value), err
+}
+
+func (s *Store) Delete(key string) error {
+	input := &dynamodb.DeleteItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			"key": {
+				S: aws.String(key),
+			},
+		},
+		TableName: &s.name,
+	}
+
+	_, err := s.dynamo.DeleteItem(input)
+	return err
+}
